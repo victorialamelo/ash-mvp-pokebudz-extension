fe03095204942d96161cc5945caf1289d1f23d1c
victorialamelo <victoria.lamelo@gmail.com>
2025-02-26 18:25:42 +0100

    changed imports names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import QuizPage from "./pages/StartQuiz"; // import the new start page
+
+import QuizPage from "./pages/QuizPage"; // import the new start page
 import HomePage from "./pages/HomePage"; // import HomePage
-import LoginPage from "./pages/Login"; // import Login page
-import AdoptedPokemonPage from "./pages/AdoptedPokemonCard"; // import Login page
+import LoginPage from "./pages/LoginPage"; // import Login page
+import AdoptedPokemonPage from "./pages/AdoptedPokemonPage"; // import Login page
 
 function App() {
   return (
